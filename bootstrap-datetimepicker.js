e8359c0d0d85754bd6b4ca9b7ab2c119e0f33387
hirarashid <60527393+hirarashid@users.noreply.github.com>
2021-12-08 10:33:47 -0800

    Delete bootstrap-datetimepicker.js

--- bootstrap-datetimepicker.js
+++
@@ -1,2 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>VQZ8QYD18Q65B6W0</RequestId><HostId>2yR6ToYwp+kp3kCgPXjnjvzsxlIFB8ihzE1MSFs99kQNv+0mg32lX8I2q8/H8PqbCloPugaRcUI=</HostId></Error>
